47c346f62b6649206e6199d6f56f75ef81ad6b62
Christian Gil <christian.a.gil@gmail.com>
2016-12-10 16:28:22 -0500

    deleted login page

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,10 @@
 const base = require('./base')
 const users = require('./users')
 const websockets = require('./websockets.js')
-const login = require('./login.js')
 const admin = require('./admin.js')
 
 module.exports = [].concat(base,
                            users,
                            websockets,
-                           login,
                            admin
                            )
