948c6c5c7f50fec0e0834e02858e91f661ad8d90
Christian Gil <christian.a.gil@gmail.com>
2017-01-07 14:06:43 -0500

    changed mongodb conection string

--- server/users/user-db.js
+++ server/users/user-db.js
@@ -2,7 +2,8 @@ var Mongoose = require('mongoose')
 var config = require('../config/auth.js').user_db
 Mongoose.Promise = require('bluebird')
 
-Mongoose.connect('mongodb://' + config.host + '/' + config.db)
+/*Mongoose.connect('mongodb://' + config.host + '/' + config.db)*/
+Mongoose.connect('mongodb://'+config.username+':'+config.password+'@'+config.host+':'+config.port+'/'+config.db+'?authSource='+config.auth.authdb)
 var db = Mongoose.connection
 db.on('error', console.error.bind(console, 'Connection with user database error'))
 
