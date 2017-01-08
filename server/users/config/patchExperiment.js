494e0607cf8f0a23024bfcc290b42a62beca5a20
Christian Gil <christian.a.gil@gmail.com>
2017-01-07 20:15:58 -0500

    changed experiment duration to minutes

--- server/users/config/patchExperiment.js
+++ server/users/config/patchExperiment.js
@@ -19,8 +19,8 @@ module.exports = {
       name: Joi.string().regex(/^[a-zA-Z0-9 ]{3,30}$/).min(3).max(30).description('Experiment Name').example('Arduino Car'),
       university: Joi.string().regex(/^[a-zA-Z0-9 ]{3,30}$/).min(3).max(30).description('Experiment University').example('Yale University'),
       url: Joi.string().min(3).max(30).description('The Experiment Url').example('http://google.com'),
-      schedule: Joi.array().items(Joi.number()).description('The experiment Allowed Hours').example('[8,9,10,11,14,15,16,17,18]'),
-      duration: Joi.array().items(Joi.number()).description('The experiment Allowed duration in hours').example('[1,2]'),
+      schedule: Joi.array().items(Joi.number()).description('The experiment Allowed initial Hours').example('[8,9,10,11,14,15,16,17,18]'),
+      duration: Joi.array().items(Joi.number().multiple(30)).required().description('The experiment Allowed durations in Minutes (must be multiples of 30)').example('[30,60,90,120]'),
       enabled: Joi.boolean().description('Wheter the Experiment is enabled to registration').example(true)
     })
   },
