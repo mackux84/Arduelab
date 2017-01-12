7e71ddace8e55f0e7fb86448c230e00430913f20
Christian Gil <christian.a.gil@gmail.com>
2017-01-12 16:17:09 -0500

    changes to university lenght

--- server/users/schemas/createUser.js
+++ server/users/schemas/createUser.js
@@ -5,7 +5,7 @@ const Joi = require('joi')
 const createUserSchema = Joi.object({
   username:   Joi.string().regex(/^[a-zA-Z0-9 ]{3,30}$/).min(3).max(30).required().description('El nombre de usuario debe de ser unico').example('andresvega'),
   email:      Joi.string().email().required().description('Un correo valido (La confirmacion sera enviada por correo electronico)').example('andresvega@email.com'),
-  university: Joi.string().regex(/^[a-zA-Z0-9 ]{3,60}$/).min(3).max(30).required().description('El usuario de la uniersidad').example('Yale'),
+  university: Joi.string().regex(/^[a-zA-Z0-9 ]{3,60}$/).min(3).max(60).required().description('El usuario de la uniersidad').example('Yale'),
   password:   Joi.string().required().description('Una contraseña segura es recomendada').example('AV1578sj'),
 }).label('Create user Schema')
 
--- server/users/schemas/updateUser.js
+++ server/users/schemas/updateUser.js
@@ -6,7 +6,7 @@ Joi.objectId = require('joi-objectid')(Joi)
 const payloadSchema = Joi.object({
   username: Joi.string().regex(/^[a-zA-Z0-9 ]{3,30}$/).min(2).max(30).description('El nombre de usuario debe de ser unico').example('andresvega').optional(),
   email: Joi.string().email().description('Un correo valido (La confirmacion sera enviada por correo electronico)').example('andresvega@email.com'),
-  university: Joi.string().regex(/^[a-zA-Z0-9 ]{3,60}$/).min(2).max(30).description('El usuario de la universidad').example('Yale'),
+  university: Joi.string().regex(/^[a-zA-Z0-9 ]{3,60}$/).min(2).max(60).description('El usuario de la universidad').example('Yale'),
   scope: Joi.string().allow('User', 'Premium', 'Admin').description('The account scope, enum').example('User'),
   isVerified: Joi.boolean().description('Si el usuario tiene un correo valido').example('true')
 }).label('Update User')
