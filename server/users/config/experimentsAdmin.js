f707b8840780f8acd1095542350bc6cfc03dde0f
Christian Gil <christian.a.gil@gmail.com>
2017-01-12 23:02:51 -0500

    modified schedule array function

--- server/users/config/experimentsAdmin.js
+++ server/users/config/experimentsAdmin.js
@@ -28,7 +28,7 @@ module.exports = {
             university: Joi.string().regex(/^[a-zA-Z0-9 ]{3,30}$/).min(3).max(30).required().description('Experiment University').example('Yale University'),
             url: Joi.string().min(3).max(30).required().description('The Experiment Url').example('http://google.com'),
             days: Joi.array().items(Joi.number().min(0).max(6)).required().description('The experiment Allowed Days in UTC format, Sunday is 0, Monday is 1, and so on.').example('[1,2,3,4]'),
-            schedule: Joi.array().items(Joi.number()).required().description('The experiment Allowed initial Hours').example('[8,9,10,11,14,15,16,17,18]'),
+            schedule: Joi.array().length(2).items(Joi.number().min(0).max(23)).required().description('The experiment active hours (initial,, final)').example('[8,18]'),
             duration: Joi.array().items(Joi.number().multiple(30)).required().description('The experiment Allowed durations in Minutes (must be multiples of 30)').example('[30,60,90,120]'),
             enabled: Joi.boolean().required().description('Wheter the Experiment is enabled to registration').example(true),
             description: Joi.string().description('Experiment Description').example('Program an RGB LED with Arduino!!')
