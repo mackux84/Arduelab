6bd1b499ecb31e0a0d18bd502613ad99ded10135
Christian Gil <christian.a.gil@gmail.com>
2017-02-15 13:39:38 -0500

    added experimental try catch

--- server/users/user-db.js
+++ server/users/user-db.js
@@ -1,15 +1,23 @@
 var Mongoose = require('mongoose')
 var config = require('../config/auth.js').user_db
 Mongoose.Promise = require('bluebird')
+var connected = false
+while (!connected) {
+  try {
+    /*Mongoose.connect('mongodb://' + config.host + '/' + config.db)*/
+    Mongoose.connect('mongodb://' + config.username + ':' + config.password + '@' + config.host + ':' + config.port + '/' + config.db)
+    var db = Mongoose.connection
+    db.on('error', console.error.bind(console, 'Connection with user database error'))
 
-/*Mongoose.connect('mongodb://' + config.host + '/' + config.db)*/
-Mongoose.connect('mongodb://'+config.username+':'+config.password+'@'+config.host+':'+config.port+'/'+config.db)
-var db = Mongoose.connection
-db.on('error', console.error.bind(console, 'Connection with user database error'))
-
-db.once('open', function callback () {
-  console.log('Connection with user database succeeded.')
-})
-
+    db.once('open', function callback() {
+      console.log('Connection with user database succeeded.')
+    })
+    connected = true
+  }
+  catch(error){
+    //connected = false
+    console.log(error)
+  }
+}
 exports.Mongoose = Mongoose
 exports.db = db
