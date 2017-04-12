2f7e223ae6a2ca777421c38fcd8392cd6cd5c295
christian gil florez <christian gil florez>
2017-04-11 20:04:13 -0500

    allow for search single creator

--- server/users/handlers/getAllCreators.js
+++ server/users/handlers/getAllCreators.js
@@ -4,19 +4,37 @@ const Boom = require('boom')
 const Creator = require('../models/Creator')
 
 module.exports = function (request, reply) {
-  Creator
-    .find()
-    // Deselect the password and version fields
-    .select('-__v')
-    .exec((error, creators) => {
-      if (error) {
-        reply(Boom.badRequest(error))
-        return
-      }
-      if (!creators.length) {
-        reply(Boom.notFound('NO SE ENCONTRARON CREADORES'))
-        return
-      }
-      reply(creators)
-    })
+  if (request.payload.identification !== '') {
+    Creator
+      .find({identification: request.payload.identification})
+      // Deselect the password and version fields
+      .select('-__v')
+      .exec((error, creators) => {
+        if (error) {
+          reply(Boom.badRequest(error))
+          return
+        }
+        if (!creators.length) {
+          reply(Boom.notFound('NO SE ENCONTRARON CREADORES'))
+          return
+        }
+        reply(creators)
+      })
+  } else {
+    Creator
+      .find()
+      // Deselect the password and version fields
+      .select('-__v')
+      .exec((error, creators) => {
+        if (error) {
+          reply(Boom.badRequest(error))
+          return
+        }
+        if (!creators.length) {
+          reply(Boom.notFound('NO SE ENCONTRARON CREADORES'))
+          return
+        }
+        reply(creators)
+      })
+  }
 }
